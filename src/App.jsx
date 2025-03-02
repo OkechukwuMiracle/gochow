3f20e70b35fdc0cac490dae0610318f3cb2842e5
OkechukwuMiracle <damianmiracle18@gmail.com>
2025-03-02 23:26:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './components/Home'
+import AboutUs from './components/AboutUs'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+export default App
+
